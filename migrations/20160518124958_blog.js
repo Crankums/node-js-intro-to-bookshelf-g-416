013d511b41d9a0a6da24feb3049d5955dbfa920e
Craig_Donavin <crdonavin@gmail.com>
2020-12-02 21:42:05 -0600

    install packages, write users migration

--- migrations/20160518124958_blog.js
+++ migrations/20160518124958_blog.js
@@ -1,8 +1,16 @@
 
 exports.up = function(knex, Promise) {
-  
+  return Promise.all([
+    knex.schema.createTableIfNotExists('users', (tbl) => {
+        tbl.increments('id').primary(),
+        tbl.string('name'),
+        tbl.string('username'),
+        tbl.string('email'),
+        tbl.timestamps()
+    })
+  ])
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTable('users')
 };
