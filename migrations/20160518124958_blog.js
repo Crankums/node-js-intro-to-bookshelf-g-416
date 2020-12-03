fe00fb7fb308c20f5277498b2ee87e464251ee8e
Craig_Donavin <crdonavin@gmail.com>
2020-12-02 22:18:59 -0600

    dropped tables and remigrated

--- migrations/20160518124958_blog.js
+++ migrations/20160518124958_blog.js
@@ -2,15 +2,24 @@
 exports.up = function(knex, Promise) {
   return Promise.all([
     knex.schema.createTableIfNotExists('users', (tbl) => {
-        tbl.increments('id').primary(),
-        tbl.string('name'),
-        tbl.string('username'),
-        tbl.string('email'),
-        tbl.timestamps()
-    })
+        tbl.increments('id').primary();
+        tbl.string('name');
+        tbl.string('username');
+        tbl.string('email');
+        tbl.timestamps();
+    }),
+    knex.schema.createTableIfNotExists('posts', (tbl) => {
+      tbl.increments().primary();
+      tbl.string('title');
+      tbl.string('body');
+      tbl.integer('author').references('users.id');
+      tbl.timestamps();
+    }),
   ])
 };
 
 exports.down = function(knex, Promise) {
-  return knex.schema.dropTable('users')
+  return knex.schema
+    .dropTable('users')
+    .dropTable('posts')
 };
